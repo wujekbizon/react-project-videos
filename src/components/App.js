4783260da8fa925b64669b9a092f689ddb69ba64
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-05-13 19:38:09 +0200

    small fix test add more style

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,20 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit('MTG ');
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term,
       },
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
@@ -24,11 +31,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
     <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img
         className="ui image"
-        alt={video.snippet.description}
+        alt={video.snippet.title}
         src={video.snippet.thumbnails.medium.url}
       />
       <div className="content">
